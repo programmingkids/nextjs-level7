bfdfb844fffe5abff585e5977cf5571d8a800c00
Hiroshi Fujiwara <fujiwara@no1s.biz>
2024-11-30 10:55:17 +0000

    added chapter02 seed

---
+++ chapter02/src/seedData.ts
@@ -0,0 +1,60 @@
+const data = [
+  {
+    id: 1,
+    name: "Micky",
+    hp: 10,
+    mp: 20,
+    job: "勇者",
+  },
+  {
+    id: 2,
+    name: "Minny",
+    hp: 20,
+    mp: 40,
+    job: "歌手",
+  },
+  {
+    id: 3,
+    name: "Donald",
+    hp: 20,
+    mp: 40,
+    job: "遊び人",
+  },
+  {
+    id: 4,
+    name: "Daizy",
+    hp: 20,
+    mp: 10,
+    job: "シェフ",
+  },
+  {
+    id: 5,
+    name: "Anna",
+    hp: 100,
+    mp: 50,
+    job: "勇者",
+  },
+  {
+    id: 6,
+    name: "Elsa",
+    hp: 100,
+    mp: 200,
+    job: "魔法使い",
+  },
+  {
+    id: 7,
+    name: "Ariel",
+    hp: 10,
+    mp: 10,
+    job: "水泳選手",
+  },
+  {
+    id: 8,
+    name: "Bell",
+    hp: 200,
+    mp: 50,
+    job: "踊り子",
+  },
+];
+
+export default data;
